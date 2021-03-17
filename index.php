599513b443dd53e061e130f80325c983dc7c6b79
davehodls <34919864+davehodls@users.noreply.github.com>
2021-03-16 20:50:08 -0600

    added Step 2 client specifies two numbers

--- index.php
+++ index.php
@@ -47,3 +47,17 @@ for ($i = 1; $i <= 120; $i++) {
     // ah well
 }
 */
+
+// Step 2: Make it so the client can specify their own two line numbers to determine the text.
+
+parseLineNumbers(5, 9);
+
+function parseLineNumbers($num1, $num2) {
+    for ($i = 1; $i <= 120; $i++) {
+        if ($i % $num1 == 0 && $i % $num2 == 0) echo 'ChiCken and SteAk';
+        else if ($i % $num1 == 0) echo 'ChiCken';
+        else if ($i % $num2 == 0) echo 'SteAk';
+        else echo $i;
+        echo "\n";
+    }
+}
